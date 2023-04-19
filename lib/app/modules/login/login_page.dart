be0877dbb8666fe8924cc14e85e2d2bec0415b69
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-04-18 21:10:46 -0300

    build login page

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -5,9 +5,37 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('login')),
-      body: Container(),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color(0XFF0092B9),
+            Color(0XFF0167B2),
+          ]),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png'),
+              SizedBox(height: screenSize.height * .1),
+              SizedBox(
+                width: screenSize.width * .8,
+                height: 49,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[200],
+                  ),
+                  onPressed: () {},
+                  child: Image.asset('assets/images/google.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
