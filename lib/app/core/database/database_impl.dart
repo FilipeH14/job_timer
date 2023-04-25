35269798bd84c997fca548b1e5d90d3a6647c573
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-04-25 20:05:12 -0300

    correcting error

---
+++ lib/app/core/database/database_impl.dart
@@ -0,0 +1,21 @@
+import 'package:isar/isar.dart';
+import 'package:job_timer/app/core/database/database.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseImpl implements Database {
+  Isar? _databaseInterface;
+
+  @override
+  Future<Isar> openConnection() async {
+    if (_databaseInterface == null) {
+      final dir = await getApplicationSupportDirectory();
+      _databaseInterface = await Isar.open(
+        [],
+        directory: dir.path,
+        inspector: true,
+      );
+    }
+
+    return _databaseInterface!;
+  }
+}
