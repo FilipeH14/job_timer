02a7a10547d3e28094161a985b487c9f0fa4c670
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-04-29 21:12:50 -0300

    builld form new project page

--- lib/app/modules/project/register/project_register_page.dart
+++ lib/app/modules/project/register/project_register_page.dart
@@ -1,13 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
 
-class ProjectRegisterPage extends StatelessWidget {
+class ProjectRegisterPage extends StatefulWidget {
   const ProjectRegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProjectRegisterPage> createState() => _ProjectRegisterPageState();
+}
+
+class _ProjectRegisterPageState extends State<ProjectRegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _nameEc = TextEditingController();
+  final _estimateEc = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameEc.dispose();
+    _estimateEc.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('New project')),
-      body: Container(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          'Criar novo projeto',
+          style: TextStyle(color: Colors.black),
+        ),
+        iconTheme: const IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _nameEc,
+                decoration: const InputDecoration(
+                  label: Text('Nome do projeto'),
+                ),
+                validator: Validatorless.required('Nome obrigatório'),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _estimateEc,
+                decoration: const InputDecoration(
+                  label: Text('Estimativa de horas'),
+                ),
+                keyboardType: TextInputType.number,
+                validator: Validatorless.multiple([
+                  Validatorless.required('Estimativa obrigatória'),
+                  Validatorless.number('Permitido somente números'),
+                ]),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 49,
+                child: ElevatedButton(
+                  child: const Text('Salvar'),
+                  onPressed: () {
+                    final formValid =
+                        _formKey.currentState?.validate() ?? false;
+
+                    if (formValid) {}
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
