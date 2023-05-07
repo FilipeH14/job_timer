581ace43cc88b5ecd29c13aac880117b66b31087
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-05-06 21:01:16 -0300

    adding task widgets on project detail screen

--- lib/app/modules/project/detail/project_detail_page.dart
+++ lib/app/modules/project/detail/project_detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:job_timer/app/modules/project/detail/widgets/project_detail_appbar.dart';
 import 'package:job_timer/app/modules/project/detail/widgets/project_pie_chart.dart';
+import 'package:job_timer/app/modules/project/detail/widgets/project_task_tile.dart';
 
 class ProjectDetailPage extends StatelessWidget {
   const ProjectDetailPage({super.key});
@@ -18,6 +19,20 @@ class ProjectDetailPage extends StatelessWidget {
                   padding: EdgeInsets.only(top: 50, bottom: 50),
                   child: ProjectPieChart(),
                 ),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
+                const ProjectTaskTile(),
               ],
             ),
           ),
